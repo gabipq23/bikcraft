c611ae0a737986db270e77f57a21ea34357dd223
Gabriela PQ <gabrielapquintaes@gmail.com>
2023-09-28 14:17:37 -0300

    atualizações de acessibilidade

--- js/script.js
+++ js/script.js
@@ -25,3 +25,25 @@ function ativarProduto(parametro){
 }
 
 parametros.forEach(ativarProduto);
+
+// perguntas frequentes; melhorando a cessibilidade
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta);
+    
+}
+
+perguntas.forEach(eventosPerguntas);
+
